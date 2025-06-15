f16b4b3f051dff4127ce30fb1ae2196985339d21
Kael-web7 <miqueiasl.ezequiel512@gmail.com>
2025-06-15 19:48:32 -0300

    fix: resolve eslint issues

--- src/app/dashboard-master/CampaignButtons.tsx
+++ src/app/dashboard-master/CampaignButtons.tsx
@@ -1,11 +1,5 @@
 import React from 'react'
 import './CampaignButtons.css'
-import { Cinzel } from 'next/font/google'
-
-const cinzel = Cinzel({
-	subsets: ['latin'],
-	weight: ['700'],
-})
 
 const CampaignButtons: React.FC = () => {
 	return (
